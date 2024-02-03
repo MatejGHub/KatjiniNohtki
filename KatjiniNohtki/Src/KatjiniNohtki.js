e44853c1ca35581fde4cd4d11e4a9e66f56a1530
Matej Stremfelj <matej.stremfelj26@gmail.com>
2024-02-03 09:59:19 +0100

    Added fixed arrow-to-top button

--- KatjiniNohtki/Src/KatjiniNohtki.js
+++ KatjiniNohtki/Src/KatjiniNohtki.js
@@ -7,6 +7,16 @@ const logoBtn = document.querySelector(".logo-image");
 const storitveSection = document.querySelector(".storitve-section");
 const kontaktSection = document.querySelector(".kontakt-section");
 
+const arrowToTopBtn = document.querySelector(".arrow-top-section");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1000) {
+    arrowToTopBtn.classList.add("active");
+  } else {
+    arrowToTopBtn.classList.remove("active");
+  }
+});
+
 storitveBtn.addEventListener("click", () => {
   storitveSection.scrollIntoView({ behavior: "smooth" });
   window.scrollBy(0, -55);
@@ -18,5 +28,25 @@ kontaktBtn.addEventListener("click", () => {
 });
 
 logoBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+const hamburgerBtn = document.querySelector("#hamburger-menu");
+const closeBtn = document.querySelector(".close-icon");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+hamburgerBtn.addEventListener("click", () => {
+  hamburgerBtn.classList.add("active");
+  hamburgerMenu.classList.add("active");
+  closeBtn.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  closeBtn.classList.remove("active");
+  hamburgerMenu.classList.remove("active");
+  hamburgerBtn.classList.remove("active");
+});
+
+arrowToTopBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
