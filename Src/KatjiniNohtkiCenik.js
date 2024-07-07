8e1e1982e5669b3135f7c6a5deb07509cfa86783
Matej Stremfelj <matej.stremfelj26@gmail.com>
2024-07-07 13:55:59 +0200

    First Commit

---
+++ Src/KatjiniNohtkiCenik.js
@@ -0,0 +1,21 @@
+const hamburgerBtn = document.querySelector("#hamburger-menu");
+const closeBtn = document.querySelector(".close-icon");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+hamburgerBtn.addEventListener("click", () => {
+  hamburgerBtn.classList.add("active");
+  hamburgerMenu.classList.add("active");
+  closeBtn.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  closeBtn.classList.remove("active");
+  hamburgerMenu.classList.remove("active");
+  hamburgerBtn.classList.remove("active");
+});
+
+const storitveBtn = document.querySelector(".storitve");
+
+storitveBtn.addEventListener("click", () => {
+  window.open("KatjiniNohtki.html#storitve-section", "_self");
+});
